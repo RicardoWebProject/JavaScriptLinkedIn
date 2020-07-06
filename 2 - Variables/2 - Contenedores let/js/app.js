fda6708c6bed7087b1be21d9b94d9b51a3ec9fb0
RicardoWebProject <luisrijhn09@gmail.com>
2020-07-06 13:22:06 -0600

    Agregados algunos comentarios y sintaxis en el documento

--- 2 - Variables/2 - Contenedores let/js/app.js
+++ 2 - Variables/2 - Contenedores let/js/app.js
@@ -1,17 +1,18 @@
 "use strict"
 
 var nombre = 'Sergio';
+//Cuando se quiera trabajar con una variable GLOBAL, se puede implementar VAR
 
 console.log(nombre)
 
 function saludo() {
-  let nombre = 'yacafx';
-    console.log(nombre)
+  let nombre = 'yacafx'; //Si queremos trabajar con una variable dentro de una función o bloque de código en específico, utilizaríamos LET.
+  console.log(nombre)
     
- let edad = 34; 
+  let edad = 34; 
   console.log(edad)
 }
 
- console.log(edad)
+console.log(edad)
 
 saludo();
