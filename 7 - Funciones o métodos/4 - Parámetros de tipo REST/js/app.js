41982af791185b422f0ebeba400334a20c64f1d3
RicardoWebProject <luisrijhn09@gmail.com>
2020-07-06 18:52:43 -0600

    Comentadas líneas de código de ejemplo

--- 7 - Funciones o métodos/4 - Parámetros de tipo REST/js/app.js
+++ 7 - Funciones o métodos/4 - Parámetros de tipo REST/js/app.js
@@ -4,8 +4,8 @@
 //*** Parámetros REST
 
 function cocinar(...masIngredientes) {
-    console.log('Ingrediente1: ', ingrediente1);
-    console.log('Ingrediente2: ', ingrediente2);
+    // console.log('Ingrediente1: ', ingrediente1);
+    // console.log('Ingrediente2: ', ingrediente2);
     console.log("masIngredientes", masIngredientes);
 }
 
